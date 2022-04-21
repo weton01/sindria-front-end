58bcf113248d781eff04c1e8b9a61021969f2f7e
Cyber-SubFlame <wesley9983@gmail.com>
2022-04-21 15:47:21 -0300

    update nexst.config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   onDemandEntries: {
     // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
+    maxInactiveAge: 10 * 60 * 1000,
     // number of pages that should be kept simultaneously without being disposed
     pagesBufferLength: 2,
   },
