a57703e07987eb06b6e9a0cb3c19d9e059107cc7
Cyber-SubFlame <wesley9983@gmail.com>
2022-06-25 13:13:18 -0300

    [refac] new menu profile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,7 +7,7 @@ import Container from "../Container";
 import FlexBox from "../FlexBox";
 import Icon from "../icon/Icon";
 import SearchBox from "../search-box/SearchBox";
-import { Tiny } from "../Typography";
+import Typography, { Small, Tiny } from "../Typography";
 import StyledHeader from "./HeaderStyle";
 import NavLink from "../nav-link/NavLink";
 import { userLogout } from "store/actions/user/actions";
@@ -16,53 +16,50 @@ import MenuItem from "@component/MenuItem";
 import Menu from "@component/Menu";
 import { useSelector } from "react-redux";
 import { destroyCookie } from "nookies";
+import { colors } from "@utils/themeColors";
+import Divider from "@component/Divider";
 
 type HeaderProps = {
   isFixed?: boolean;
   className?: string;
 };
 
-const Header: React.FC<HeaderProps> = ({
-  isFixed,
-  className,
-}) => {
-  const [products, setProducts] = useState([])
-  const [productsQuantity, setProductsQuantity] = useState(0)
+const Header: React.FC<HeaderProps> = ({ isFixed, className }) => {
+  const [products, setProducts] = useState([]);
+  const [productsQuantity, setProductsQuantity] = useState(0);
 
   const user = useAppSelector((state) => state?.user);
   const dispatch = useAppDispatch();
 
   const onClickLogout = () => {
     dispatch(userLogout());
-    destroyCookie(undefined, 'shop_token', {
-      maxAge: 60 * 60  * 7,
-      path: '/'
-    })
+    destroyCookie(undefined, "shop_token", {
+      maxAge: 60 * 60 * 7,
+      path: "/",
+    });
   };
 
-  const orderStores = useSelector((selec: any) =>
-    selec?.cart?.orderStores
-  )
+  const orderStores = useSelector((selec: any) => selec?.cart?.orderStores);
 
   useEffect(() => {
     let newProducts = [];
 
-    orderStores?.forEach(ost => {
-      newProducts = [...newProducts, ...ost.orderProducts]
-    })
+    orderStores?.forEach((ost) => {
+      newProducts = [...newProducts, ...ost.orderProducts];
+    });
 
-    setProducts(newProducts)
-  }, [orderStores, setProducts])
+    setProducts(newProducts);
+  }, [orderStores, setProducts]);
 
   useEffect(() => {
     let total: number = 0;
 
-    products.forEach(p => {
-      total += p.quantity
-    })
+    products.forEach((p) => {
+      total += p.quantity;
+    });
 
-    setProductsQuantity(total)
-  }, [products])
+    setProductsQuantity(total);
+  }, [products]);
 
   return (
     <StyledHeader className={className}>
@@ -107,9 +104,101 @@ const Header: React.FC<HeaderProps> = ({
                 </FlexBox>
               }
             >
-              <MenuItem key={"profile"}>Perfil</MenuItem>
+              <Link href={"/profile"}>
+                <MenuItem key={"profile"} style={{ width: 332 }}>
+                  <FlexBox gap={16} alignItems={"center"} width="100%">
+                    <IconButton bg="gray.200" p="18px">
+                      <Icon variant="small">user-2</Icon>
+                    </IconButton>
+                    <FlexBox flexDirection={"column"}>
+                      <Typography fontSize={16} fontWeight={600}>
+                        Wesley Campana Ferreira
+                      </Typography>
+                      <Small>Veja seu perfil</Small>
+                    </FlexBox>
+                  </FlexBox>
+                </MenuItem>
+              </Link>
+              <Divider bg={colors.gray["300"]} />
+              <Link href={"/orders"}>
+                <MenuItem key={"order"}>
+                  <FlexBox gap={16} alignItems={"center"} width="100%">
+                    <IconButton bg="gray.200" p="8px">
+                      <Icon variant="small" size="16px">
+                        bag
+                      </Icon>
+                    </IconButton>
+                    <Typography fontSize={14} fontWeight={600}>
+                      Todos pedidos
+                    </Typography>
+                  </FlexBox>
+                </MenuItem>
+              </Link>
+              <Link href={"/coupons"}>
+                <MenuItem key={"coupon"}>
+                  <FlexBox gap={16} alignItems={"center"} width="100%">
+                    <IconButton bg="gray.200" p="8px">
+                      <Icon variant="small" size="16px">
+                        coupon
+                      </Icon>
+                    </IconButton>
+                    <Typography fontSize={14} fontWeight={600}>
+                      Cupons
+                    </Typography>
+                  </FlexBox>
+                </MenuItem>
+              </Link>
+              <Link href={"/wish-list"}>
+                <MenuItem key={"wish-list"}>
+                  <FlexBox gap={16} alignItems={"center"} width="100%">
+                    <IconButton bg="gray.200" p="8px">
+                      <Icon variant="small" size="16px">
+                        heart
+                      </Icon>
+                    </IconButton>
+                    <Typography fontSize={14} fontWeight={600}>
+                      Lista de desejos
+                    </Typography>
+                  </FlexBox>
+                </MenuItem>
+              </Link>
+              <Link href={"/favorite-stores"}>
+                <MenuItem key={"favorite-stores"}>
+                  <FlexBox gap={16} alignItems={"center"} width="100%">
+                    <IconButton bg="gray.200" p="8px">
+                      <Icon variant="small" size="16px">
+                        bookmark
+                      </Icon>
+                    </IconButton>
+                    <Typography fontSize={14} fontWeight={600}>
+                      Lojas favoritas
+                    </Typography>
+                  </FlexBox>
+                </MenuItem>
+              </Link>
+              <MenuItem key={"feedback"}>
+                <FlexBox gap={16} alignItems={"center"} width="100%">
+                  <IconButton bg="gray.200" p="8px">
+                    <Icon variant="small" size="16px">
+                      feedback-thumbs-up
+                    </Icon>
+                  </IconButton>
+                  <Typography fontSize={14} fontWeight={600}>
+                    Dar feedback
+                  </Typography>
+                </FlexBox>
+              </MenuItem>
               <MenuItem key={"logout"} onClick={onClickLogout}>
-                Sair
+                <FlexBox gap={16} alignItems={"center"} width="100%">
+                  <IconButton bg="gray.200" p="8px">
+                    <Icon variant="small" size="16px">
+                      log-out
+                    </Icon>
+                  </IconButton>
+                  <Typography fontSize={14} fontWeight={600}>
+                    Sair
+                  </Typography>
+                </FlexBox>
               </MenuItem>
             </Menu>
           ) : (
